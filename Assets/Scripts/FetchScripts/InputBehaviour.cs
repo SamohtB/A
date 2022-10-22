a95223dd3289033eee2a0503c777ab6e6b9706d6
Kappa <kevin_carl_capalar@dlsu.edu.ph>
2022-10-22 15:06:41 +0800

    Force Mechanic - applied
    When collide with an object it will push back by the player (dog)

--- Assets/Scripts/FetchScripts/InputBehaviour.cs
+++ Assets/Scripts/FetchScripts/InputBehaviour.cs
@@ -5,19 +5,17 @@ using UnityEngine;
 
 public class InputBehaviour : MonoBehaviour
 {
-     
+    //Default parameters
     [SerializeField] private float speed = 10.0f;
     [SerializeField] Animator animator;
 
-    [SerializeField] private Vector3 appliedForce = new Vector3(0, 50.0f, 0);
+    [SerializeField] private Vector3 appliedForce;
 
-    const float BUFFER = 0.5f;
+    
 
     private float currTime;
     private Transform transformObj;
     private Rigidbody rigidbody;
-
-
 
     private bool isOnGround = true;
 
@@ -25,12 +23,13 @@ public class InputBehaviour : MonoBehaviour
     bool isMoving;
     bool isJumping;
 
+    //Rigidbody Force
+    const float BUFFER = 0.5f; //time between jumps
+    [SerializeField] private float forceMultiplier;
+
     private void Awake()
-    {
-        
-       
+    {    
         HandleMovement();
-        
     }
 
     // Start is called before the first frame update
@@ -52,7 +51,7 @@ public class InputBehaviour : MonoBehaviour
 
     void MovementManager()
     {
-
+        //Vertical Movement
         float forwardMovement = Input.GetAxisRaw("Vertical");
         if (!Input.GetKey(KeyCode.W) &&  !Input.GetKey(KeyCode.S))
         {
@@ -66,9 +65,11 @@ public class InputBehaviour : MonoBehaviour
             animator.SetBool("isMoving", true);
             
         }
-        //Debug.Log($"Speed: {forwardMovement}");
+        //Player Rotation 
         transformObj.Rotate(new Vector3(0, 1.0f, 0), 200.0f * Time.deltaTime * Input.GetAxisRaw("Horizontal"));
 
+
+        //Player Jump
         if (Input.GetKeyDown("space") && isOnGround == true && BUFFER < currTime)
         {
             Debug.Log("Working Force");
@@ -83,6 +84,7 @@ public class InputBehaviour : MonoBehaviour
 
     }
 
+    //Animation
     void HandleMovement()
     {
         bool isMoving = animator.GetBool("isMoving");
@@ -93,9 +95,26 @@ public class InputBehaviour : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+
+        //Jump detection
         isOnGround = true;
         animator.SetBool("isJumping", false);
         isJumping = false;
+
+        //check for any object that is can be moved
+        if(collision.gameObject.GetComponent<Rigidbody>() != null)
+        {
+            //parameters
+            Vector3 currPos = this.transform.position;
+            Vector3 collidePos = collision.transform.position;
+
+            Vector3 pushForce = Vector3.Normalize(currPos - collidePos);
+            Rigidbody collideRigidBody = collision.gameObject.GetComponent<Rigidbody>();
+
+
+            collideRigidBody.AddForce((-pushForce) * forceMultiplier, ForceMode.Impulse);
+
+        }
     }
 }
 
